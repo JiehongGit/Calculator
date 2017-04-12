cc9bccf6d7f545fed5b2f6dba1e01bc1962bfc6b
KnightGithub <974861604@qq.com>
2017-04-12 16:54:00 +0800

    实例化按钮

--- app/src/main/java/com/example/tiancai/calculator/MainActivity.java
+++ app/src/main/java/com/example/tiancai/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 //当程序里面有很多的部件需要点击执行动作的时候可以使用implements OnClickListener
@@ -30,4 +31,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn_divide;//除
 
     Button btn_equal;//等于
+
+    EditText et_input;
+    boolean clear_flag;//清空标识
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //实例化按钮
+        btn_0 = (Button) findViewById(R.id.btn_0) ;
+        btn_1 = (Button) findViewById(R.id.btn_1) ;
+        btn_2 = (Button) findViewById(R.id.btn_2) ;
+        btn_3 = (Button) findViewById(R.id.btn_3) ;
+        btn_4 = (Button) findViewById(R.id.btn_4) ;
+        btn_5 = (Button) findViewById(R.id.btn_5) ;
+        btn_6 = (Button) findViewById(R.id.btn_6) ;
+        btn_7 = (Button) findViewById(R.id.btn_7) ;
+        btn_8 = (Button) findViewById(R.id.btn_8) ;
+        btn_9 = (Button) findViewById(R.id.btn_9) ;
+        btn_point = (Button) findViewById(R.id.btn_point) ;
+        btn_clear = (Button) findViewById(R.id.btn_clear) ;
+        btn_del = (Button) findViewById(R.id.btn_del) ;
+        btn_plus = (Button) findViewById(R.id.btn_plus) ;
+        btn_minus = (Button) findViewById(R.id.btn_minus) ;
+        btn_multiply = (Button) findViewById(R.id.btn_multiply) ;
+        btn_divide = (Button) findViewById(R.id.btn_divide) ;
+        btn_equal = (Button) findViewById(R.id.btn_equal);
+
+    }
 }
